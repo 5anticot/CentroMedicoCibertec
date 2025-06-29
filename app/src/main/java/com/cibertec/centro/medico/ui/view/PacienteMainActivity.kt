7a4c7e6285d719b1e00d0fa7d3cebbfebff2873e
LifoX404 <ccorrales.dev@outlook.com>
2025-06-29 18:36:01 -0500

    feat(implementacion funcional)

--- app/src/main/java/com/cibertec/centro/medico/ui/view/PacienteMainActivity.kt
+++ app/src/main/java/com/cibertec/centro/medico/ui/view/PacienteMainActivity.kt
@@ -3,8 +3,6 @@ package com.cibertec.centro.medico.ui.view
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.ImageButton
 import android.widget.PopupMenu
